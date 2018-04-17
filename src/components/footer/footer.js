c71fd47ab1615aba98170998fd220b8f3829f56c
Carlo Bilbao <cbilbao88@gmail.com>
2018-04-17 15:30:36 -0700

    chore: update work section

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -23,9 +23,9 @@ const obFooter = () => {
                           <span className="alt-font margin-20px-right">Follow Me</span>
                           <div className="social-icon-style-8 display-inline-block vertical-align-middle">
                               <ul className="small-icon no-margin-bottom">
-                                  <li><a className="facebook text-white" href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook accent-hover"></i></a></li>
-                                  <li><a className="twitter text-white" href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter accent-hover"></i></a></li>
-                                  <li><a className="instagram text-white" href="https://instagram.com/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram no-margin-right accent-hover"></i></a></li>
+                                  <li><a className="facebook text-white" href="https://www.facebook.com/carlo.anthony.7" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook accent-hover"></i></a></li>
+                                  <li><a className="twitter text-white" href="https://twitter.com/CarloAntonioB" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter accent-hover"></i></a></li>
+                                  <li><a className="instagram text-white" href="https://instagram.com/carloantoniob/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram no-margin-right accent-hover"></i></a></li>
                                   <li><a className="google text-white" href="https://www.youtube.com/channel/UC8fnvYf5xW-8YVgJCRqSjsA" target="_blank" rel="noopener noreferrer"><i className="fa fa-youtube accent-hover"></i></a></li>
                                   <li><a className="google text-white" href="https://medium.com/@cbilbao88" target="_blank" rel="noopener noreferrer"><i className="fa fa-medium accent-hover"></i></a></li>
                               </ul>
