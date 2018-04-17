c4fc99889d1135bb44dac821749fed9f52959041
Carlo Bilbao <cbilbao88@gmail.com>
2018-04-17 15:22:40 -0700

    chore: update header and footer with appropriate data

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from '../../images/logo.svg';
 
 const obFooter = () => {
   return (
@@ -10,29 +9,25 @@ const obFooter = () => {
           <div className="container">
               <div className="row equalize xs-equalize-auto padding-30px-bottom">
                   {/* start slogan */}
-                  <div className="col-md-4 col-sm-5 col-xs-12 text-center alt-font display-table xs-text-center xs-margin-15px-bottom">
+                  <div className="col-md-12 col-sm-12 col-xs-12 text-center alt-font display-table xs-text-center xs-margin-15px-bottom">
                       <div className="display-table-cell vertical-align-middle">
-                          Bay Area Based, Photographer | Videographer
+                          Bay Area Based | Software Developer | Videographer | Aspiring Entrepreneur
                       </div>
                   </div>
                   {/* end slogan */}
-                  {/* start logo */}
-                  <div className="col-md-4 col-sm-2 col-xs-12 text-center display-table xs-margin-10px-bottom">
-                      <div className="display-table-cell vertical-align-middle">
-                          <a href="index.html"><img className="footer-logo" src={ logo } data-at2x="https://d1xrp9zhb3ks3c.cloudfront.net/web/ortabrothers/template-pofo/images/logo-white@2x.png" alt="DB Photography" /></a>
-                      </div>
-                  </div>
-                  {/* end logo */}
+              </div>
+              <div className="row equalize xs-equalize-auto padding-30px-bottom">
                   {/* start social media */}
-                  <div className="col-md-4 col-sm-5 col-xs-12 col-xs-12 text-center display-table xs-text-center">
+                  <div className="col-md-12 col-sm-12 col-xs-12 col-xs-12 text-center display-table xs-text-center">
                       <div className="display-table-cell vertical-align-middle">
                           <span className="alt-font margin-20px-right">Follow Me</span>
                           <div className="social-icon-style-8 display-inline-block vertical-align-middle">
                               <ul className="small-icon no-margin-bottom">
                                   <li><a className="facebook text-white" href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook accent-hover"></i></a></li>
                                   <li><a className="twitter text-white" href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter accent-hover"></i></a></li>
-                                  <li><a className="google text-white" href="https://plus.google.com" target="_blank" rel="noopener noreferrer"><i className="fa fa-google-plus accent-hover"></i></a></li>
                                   <li><a className="instagram text-white" href="https://instagram.com/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram no-margin-right accent-hover"></i></a></li>
+                                  <li><a className="google text-white" href="https://www.youtube.com/channel/UC8fnvYf5xW-8YVgJCRqSjsA" target="_blank" rel="noopener noreferrer"><i className="fa fa-youtube accent-hover"></i></a></li>
+                                  <li><a className="google text-white" href="https://medium.com/@cbilbao88" target="_blank" rel="noopener noreferrer"><i className="fa fa-medium accent-hover"></i></a></li>
                               </ul>
                           </div>
                       </div>
@@ -47,7 +42,7 @@ const obFooter = () => {
               <div className="footer-bottom border-top border-color-medium-dark-gray padding-30px-top">
                   <div className="row">
                       {/* start copyright */}
-                      <div className="col-md-12 col-sm-12 col-xs-12 text-center text-small xs-text-center">&copy; 2018 Darryl Baello Photography is Proudly Powered by <a className="accent-hover" href="carlobilbao.com" target="_blank" title="Carlo" rel="noopener noreferrer">Carlo Bilbao</a></div>
+                      <div className="col-md-12 col-sm-12 col-xs-12 text-center text-small xs-text-center">&copy; 2018 <a className="accent-hover" href="carlobilbao.com" target="_blank" title="Carlo" rel="noopener noreferrer">Carlo Bilbao</a></div>
                       {/* end copyright */}
                   </div>
               </div>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../../App.css';
-import logo from '../../images/logo.svg';
 
 const obHeader = () => {
   return (
@@ -10,14 +9,6 @@ const obHeader = () => {
       <nav className="navbar navbar-default white-link bootsnav bg-black header-dark nav-box-width navbar-top on no-full">
           <div className="container nav-header-container">
               <div className="row">
-                {/* start logo */}
-                <div className="col-md-2 col-xs-5">
-                    <a href="/" title="Pofo" className="logo">
-                      <img src={ logo } data-at2x={ logo } className="logo-dark" alt="Pofo" />
-                      <img src={ logo } data-at2x={ logo } className="logo-light default" alt="Pofo" />
-                    </a>
-                </div>
-                {/* end logo */}
                 <div className="col-md-7 col-xs-2 width-auto pull-right accordion-menu xs-no-padding-right">
                     <button type="button" className="navbar-toggle collapsed pull-right" data-toggle="collapse" data-target="#navbar-collapse-toggle-1">
                         <span className="sr-only">toggle navigation</span>
@@ -55,8 +46,11 @@ const obHeader = () => {
                 </div>
                 <div className="col-md-2 col-xs-5 width-auto">
                     <div className="header-social-icon xs-display-none">
-                        <a href="https://www.facebook.com/" title="Facebook" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook"></i></a>
-                        <a href="https://instagram.com" title="Instagram" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"></i></a>
+                        <a href="https://www.facebook.com/carlo.anthony.7" title="Facebook" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook"></i></a>
+                        <a href="https://instagram.com/carloantoniob/" title="Instagram" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"></i></a>
+                        <a href="https://twitter.com/CarloAntonioB" title="Twitter" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter"></i></a>
+                        <a href="https://www.youtube.com/channel/UC8fnvYf5xW-8YVgJCRqSjsA" title="YouTube" target="_blank" rel="noopener noreferrer"><i className="fa fa-youtube"></i></a>
+                        <a href="https://medium.com/@cbilbao88" title="Medium" target="_blank" rel="noopener noreferrer"><i className="fa fa-medium"></i></a>
                     </div>
                 </div>
               </div>
