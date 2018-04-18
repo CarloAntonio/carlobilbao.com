a401f298fdf65b448fe217dafb1de7e8e6a2b4a6
Carlo Bilbao <cbilbao88@gmail.com>
2018-04-17 17:49:45 -0700

    chore: update contact section of home page

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -12,12 +12,13 @@ const home = () => {
                         <div className="slider-text-middle-main">
                             <div className="slider-text-middle">
                                 <div className="col-lg-4 col-md-8 col-sm-10 col-xs-12 center-col text-center">
-                                    <span className="text-extra-small alt-font letter-spacing-2 text-uppercase margin-20px-bottom display-inline-block text-medium-gray">dolor sit amet, consectetur adipiscing</span>
-                                    <h2 className="font-weight-600 alt-font margin-40px-bottom sm-margin-20px-bottom text-extra-dark-gray letter-spacing-minus-1">Lorem ipsum dolor sit amet, consectetur elit.</h2>
+                                    <span className="text-extra-small alt-font letter-spacing-2 text-uppercase margin-20px-bottom display-inline-block text-medium-gray">Carlo Antonio Soriano Bilbao</span>
+                                    <h2 className="font-weight-600 alt-font margin-40px-bottom sm-margin-20px-bottom text-extra-dark-gray letter-spacing-minus-1">Software Developer, Science Geek, Life-Long Learner</h2>
                                     <div className="social-icon-style-8">
                                         <ul className="small-icon">
-                                            <li><a className="facebook text-extra-dark-gray margin-5px-lr" href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook accent-hover" aria-hidden="true"></i></a></li>
+                                            <li><a className="linkedin text-extra-dark-gray margin-5px-lr" href="https://www.linkedin.com/in/carlo-bilbao/" target="_blank"><i className="fa fa-linkedin accent-hover" aria-hidden="true"></i></a></li>
                                             <li><a className="dribbble text-extra-dark-gray margin-5px-lr" href="https://instagram.com/" target="_blank"><i className="fa fa-instagram accent-hover" aria-hidden="true"></i></a></li>
+                                            <li><a className="medium text-extra-dark-gray margin-5px-lr" href="https://medium.com/@cbilbao88" target="_blank"><i className="fa fa-medium accent-hover" aria-hidden="true"></i></a></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -31,101 +32,20 @@ const home = () => {
             </div>
         </section>
         {/* end hero section */}
+
         {/* about agency section */}
         <section className="big-section wow fadeIn bg-black" id="about">
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-lg-5 col-md-8 col-sm-10 col-xs-12 center-col text-center">
-                        <div className="alt-font margin-20px-bottom sm-margin-20px-bottom text-uppercase text-extra-small letter-spacing-2">Snapshot of Me</div>
-                        <h4 className="alt-font font-weight-600 text-white no-margin-bottom">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultrices feugiat lectus, dapibus tincidunt ante eleifend vitae.</h4>
+                        <div className="alt-font margin-20px-bottom sm-margin-20px-bottom text-uppercase text-extra-small letter-spacing-2">About Me</div>
+                        <h4 className="alt-font font-weight-600 text-white no-margin-bottom">Hi, I&#39;m Carlo, a web developer based in SF. I&#39;m passionate about using new technology to solve old problems.</h4>
                         <div className="alt-font text-right margin-40px-top sm-margin-20px-bottom text-uppercase text-extra-small letter-spacing-2"><a className="accent-hover" href="/about">Learn More</a></div>
                     </div>
                 </div>
             </div>
         </section>
         {/* end about agency section */}
-        {/* start photo gallery section */}
-        <section className="wow fadeIn no-padding">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-12 no-padding lightbox-portfolio">
-                        <div id="justified" className="justified-gallery">
-                            {/* start gallery item */}
-                            <div className=" wow fadeInUp">
-                                <a href="http://placehold.it/975x650" className="gallery-link"><img src="http://placehold.it/975x650" alt="Herbal Beauty Salon"/></a>
-                            </div>
-                            {/* end gallery item */}
-                            {/* start gallery item */}
-                            <div className=" wow fadeInUp" data-wow-delay="0.2s">
-                                <a href="http://placehold.it/800x650" className="gallery-link"><img src="http://placehold.it/800x650" alt="Tailoring Interior"/></a>
-                            </div>
-                            {/* end gallery item */}
-                            {/* start gallery item */}
-                            <div className=" wow fadeInUp" data-wow-delay="0.4s">
-                                <a href="http://placehold.it/488x650" className="gallery-link"><img src="http://placehold.it/488x650" alt="Pixflow Studio"/></a>
-                            </div>
-                            {/* end gallery item */}
-                            {/* start gallery item */}
-                            <div className=" wow fadeInUp" data-wow-delay="0.6s">
-                                <a href="http://placehold.it/496x650" className="gallery-link"><img src="http://placehold.it/496x650" alt="Designblast Inc"/></a>
-                            </div>
-                            {/* end gallery item */}
-                            {/* start gallery item */}
-                            <div className=" wow fadeInUp">
-                                <a href="http://placehold.it/1036x650" className="gallery-link"><img src="http://placehold.it/1036x650" alt="Harddot Stone"/></a>
-                            </div>
-                            {/* end gallery item */}
-                            {/* start gallery item */}
-                            <div className=" wow fadeInUp" data-wow-delay="0.2s">
-                                <a href="http://placehold.it/488x650" className="gallery-link"><img src="http://placehold.it/488x650" alt="Educamp School"/></a>
-                            </div>
-                            {/* end gallery item */}
-                            {/* start gallery item */}
-                            <div className=" wow fadeInUp" data-wow-delay="0.4s">
-                                <a href="http://placehold.it/1040x650" className="gallery-link"><img src="http://placehold.it/1040x650" alt="Branding Identity"/></a>
-                            </div>
-                            {/* end gallery item */}
-                            {/* start gallery item */}
-                            <div className=" wow fadeInUp">
-                                <a href="http://placehold.it/975x650" className="gallery-link"><img src="http://placehold.it/975x650" alt="Violator Series"/></a>
-                            </div>
-                            {/* end gallery item */}
-                            {/* start gallery item */}
-                            <div className=" wow fadeInUp" data-wow-delay="0.2s">
-                                <a href="http://placehold.it/974x650" className="gallery-link"><img src="http://placehold.it/974x650" alt="Third Eye Glasses"/></a>
-                            </div>
-                            {/* end gallery item */}
-                            {/* start gallery item */}
-                            <div className=" wow fadeInUp" data-wow-delay="0.4s">
-                                <a href="http://placehold.it/852x650" className="gallery-link"><img src="http://placehold.it/852x650" alt="Herbal Beauty Salon"/></a>
-                            </div>
-                            {/* end gallery item */}
-                            {/* start gallery item */}
-                            <div className=" wow fadeInUp">
-                                <a href="http://placehold.it/487x650" className="gallery-link"><img src="http://placehold.it/487x650" alt="Banana Design"/></a>
-                            </div>
-                            {/* end gallery item */}
-                            {/* start gallery item */}
-                            <div className=" wow fadeInUp" data-wow-delay="0.2s">
-                                <a href="http://placehold.it/685x650" className="gallery-link"><img src="http://placehold.it/685x650" alt="Graphic Design"/></a>
-                            </div>
-                            {/* end gallery item */}
-                            {/* start gallery item */}
-                            <div className=" wow fadeInUp" data-wow-delay="0.4s">
-                                <a href="http://placehold.it/975x650" className="gallery-link"><img src="http://placehold.it/975x650" alt="Web Interactive"/></a>
-                            </div>
-                            {/* end gallery item */}
-                            {/* start gallery item */}
-                            <div className=" wow fadeInUp" data-wow-delay="0.6s">
-                                <a href="http://placehold.it/487x650" className="gallery-link"><img src="http://placehold.it/487x650" alt="Digital Branding"/></a>
-                            </div>
-                            {/* end gallery item */}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        {/* end photo gallery section */}
 
         {/* start contact section */}
         <section class="wow fadeIn animated animated">
@@ -141,13 +61,13 @@ const home = () => {
                     <div class="col-md-7 col-sm-12 col-xs-12 wow fadeInRight" data-wow-delay="0.2s">
                         <div class="display-table width-100 height-100">
                             <div class="display-table-cell vertical-align-middle padding-twelve-lr sm-text-center sm-no-padding width-100">
-                                <span class="alt-font margin-10px-bottom display-inline-block text-medium accent-color">Ready to get capture that moment?</span>
-                                <h6 class="alt-font text-extra-dark-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dui sem, dictum quis lacus vitae, venenatis auctor mi.</h6>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since. Lorem Ipsum has been the industry's standard dummy text ever since. Lorem Ipsum is printing and typesetting simply dummy text.</p>
-                                <a href="services-simple.html" class="btn btn-dark-gray btn-small text-extra-small btn-rounded margin-5px-top"><i class="fa fa-play-circle icon-very-small margin-5px-right no-margin-left" aria-hidden="true"></i>let&#39;s do it</a>
+                                <span class="alt-font margin-10px-bottom display-inline-block text-medium accent-color">Let&#39;s build something together</span>
+                                <h6 class="alt-font text-extra-dark-gray">If you are interested in a new project, collaboration, or a reason to get drinks don&#39;t hesitate to get in contact with me.</h6>
+                                <p>I&#39;m currently available for freelance work, so if you fancy a chat, click the button below!</p>
+                                <a href="/contact" class="btn btn-dark-gray btn-small text-extra-small btn-rounded margin-5px-top"><i class="fa fa-play-circle icon-very-small margin-5px-right no-margin-left" aria-hidden="true"></i>let&#39;s do it</a>
                             </div>
                         </div>
-                    </div>s
+                    </div>
                 </div>
             </div>
         </section>
