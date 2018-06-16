d6ed87308023b75ae45e2f6aa9c092076133e36b
Carlo Bilbao <cbilbao88@gmail.com>
2018-06-15 19:12:29 -0700

    chore: fix code spacing

--- src/containers/about/about.js
+++ src/containers/about/about.js
@@ -5,6 +5,7 @@ import SliderItem from './subComp/sliderItem';
 
 //image assets
 import aboutHero from '../../assets/img/about-hero.jpg';
+import profileImg from '../../assets/img/profile.jpg'
 import { sliderData } from './data';
 
 const about = () => {
@@ -16,18 +17,20 @@ const about = () => {
         <section className="custom-pt pb-no wow fadeIn main-slider mobile-height content-right-slider">
             <div className="container-fluid">
                 <div className="row equalize sm-equalize-auto">
-                    <div className="col-md-6 cover-background sm-height-500px xs-height-350px wow fadeIn padding-twenty-five-top padding-five-left" style ={ { backgroundImage: `url(${ aboutHero })` } }>
+                    <div 
+                        className="col-md-6 cover-background sm-height-500px xs-height-350px wow fadeIn padding-twenty-five-top padding-five-left" 
+                        style ={ { backgroundImage: `url(${ aboutHero })` } }>
                         <h4 className="text-white text-standout display-block margin-5px-bottom alt-font">Hello,</h4>
                         <h6 className="text-white text-standout font-weight-300 margin-20px-bottom alt-font">I&#39;m Carlo Bilbao</h6>
-                        <p className="text-large text-white text-standout dispaly-block pull-left font-weight-300 width-90 margin-35px-bottom">I design and implement thoughtful websites for companies, businesses, and various events. I love being outdoors, creating videos, and being a gym rat.</p>
+                        <p className="text-large text-white text-standout dispaly-block pull-left font-weight-300 width-90 margin-35px-bottom">
+                            I design and implement thoughtful websites for companies, businesses, and various events. I love being outdoors, creating videos, and being a gym rat.
+                        </p>
                     </div>
                     <div className="swiper-full-screen swiper-container col-md-6 no-padding-lr white-move bg-extra-dark-gray text-center xs-padding-20px-tb wow fadeIn">
                         <div className="swiper-wrapper">
-
                             {sliderData.map(slider => {
                                 return <SliderItem slider={slider.slider} num={slider.num}/>
                             })}
-
                         </div>
                         {/* start slider pagination */}
                         <div className="swiper-button-next slider-long-arrow-white"></div>
@@ -45,12 +48,32 @@ const about = () => {
                 <div className="row">
                     <div className="col-md-12 col-sm-12 col-xs-12 xs-text-center">
 
-                        <figure className="wp-caption alignright"><img alt="" src="http://d1ty8z742i6uvq.cloudfront.net/carlobilbao/img/profile.jpg"/><figcaption className="wp-caption-text">You&#39;re looking at my portfolio page, that&#39;s awesome :)</figcaption></figure>
-                        <span className="text-medium alt-font font-weight-600 margin-20px-bottom display-block accent-color">"When figuring what type of work you should do, treat it much like the rest of life, find things that make you lose track of time."</span>
-                        <p>I am both a software developer and biotech professional based in the San Francisco Bay Area. I&#39;m passionate about using current and emerging technologies to help solve old problems. Specifically, problems that plague those that are less fortunate, marginalized, or in situations where they are not able to fix their predicament. It’s these problems and the avenues that new technology creates, that motivates me to learn and develop new skills and technologies.</p>
-                        <p>Currently, I work as a freelance web developer helping businesses and individuals expand their market by create an online presence. Additionally, I partner up with other developers to tackle larger web applications and projects. The rest of my time is spent taking classNamees to earn my master&#39;s degree in Computer Science.</p>
-                        <p>In the past, I earned my bachelor&#39;s degree in biochemistry and molecular biology at the University of California, Davis. (Go Aggies!) Professionally, I worked at a biotech company manufacturing DNA sequences for research and applied use for nearly 4 years. My love for science did not give way to technology, rather, I hope to combine them and, hopefully, have them compliment each other as I strive to help solve interesting problems that will hopefully help make people&#39;s lives better. If you&#39;d like a more detailed view of my work history feel free to check out my LinkedIn: <a href="https://www.linkedin.com/in/carlo-bilbao/" className="accent-hover" target="_blank" rel="noopener noreferrer">Here</a>.</p>
-
+                        <figure className="wp-caption alignright">
+                            <img alt="" src={ profileImg }/>
+                            <figcaption className="wp-caption-text">You&#39;re looking at my portfolio page, that&#39;s awesome :)</figcaption>
+                        </figure>
+                        <span className="text-medium alt-font font-weight-600 margin-20px-bottom display-block accent-color">
+                            "When figuring what type of work you should do, treat it much like the rest of life, find things that make you lose track of time."
+                        </span>
+                        <p>
+                            I am both a software developer and biotech professional based in the San Francisco Bay Area. 
+                            I&#39;m passionate about using current and emerging technologies to help solve old problems. 
+                            Specifically, problems that plague those that are less fortunate, marginalized, or in situations where they are not able to fix their predicament. 
+                            It’s these problems and the avenues that new technology creates, that motivates me to learn and develop new skills and technologies.
+                        </p>
+                        <p>
+                            Currently, I work as a freelance web developer helping businesses and individuals expand their market by create an online presence. 
+                            Additionally, I partner up with other developers to tackle larger web applications and projects. 
+                            The rest of my time is spent taking classNamees to earn my master&#39;s degree in Computer Science.
+                        </p>
+                        <p>
+                            In the past, I earned my bachelor&#39;s degree in biochemistry and molecular biology at the University of California, Davis. 
+                            (Go Aggies!) Professionally, I worked at a biotech company manufacturing DNA sequences for research and applied use for nearly 4 years. 
+                            My love for science did not give way to technology, rather, I hope to combine them and, hopefully, 
+                            have them compliment each other as I strive to help solve interesting problems that will hopefully help make people&#39;s lives better. 
+                            If you&#39;d like a more detailed view of my work history feel free to check out my LinkedIn: 
+                            <a href="https://www.linkedin.com/in/carlo-bilbao/" className="accent-hover" target="_blank" rel="noopener noreferrer">Here</a>.
+                        </p>
 
                     </div>
                 </div>
