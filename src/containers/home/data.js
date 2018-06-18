e5755f7f9acad870dea4ebd1d4ff6254aa40cda1
Carlo Bilbao <cbilbao88@gmail.com>
2018-06-18 11:06:12 -0700

    fix: adjust content to fix small phones

--- src/containers/home/data.js
+++ src/containers/home/data.js
@@ -6,42 +6,42 @@ export const progressData = {
     technologies: [
         {
             item: "Javascript",
-            experience: "1 year experience",
+            experience: "1 year",
             percent: "90%"
         },
         {
             item: "HTML5",
-            experience: "1 year experience",
+            experience: "1 year",
             percent: "90%"
         },
         {
             item: "CSS",
-            experience: "1 year experience",
+            experience: "1 year",
             percent: "90%"
         },
         {
             item: "Node",
-            experience: "<1 year experience",
+            experience: "<1 year",
             percent: "50%"
         },
         {
             item: "Java",
-            experience: "1 year experience",
+            experience: "1 year",
             percent: "80%"
         },
         {
             item: "XML",
-            experience: "1 year experience",
+            experience: "1 year",
             percent: "80%"
         },
         {
             item: "C++",
-            experience: "<1 year experience",
+            experience: "<1 year",
             percent: "40%"
         },
         {
             item: "Python",
-            experience: "<1 year experience",
+            experience: "<1 year",
             percent: "20%"
         },
 
@@ -49,111 +49,111 @@ export const progressData = {
     libraries: [
         {
             item: "React",
-            experience: "<1 year experience",
+            experience: "<1 year",
             percent: "80%"
         },
         {
             item: "Redux",
-            experience: "<1 year experience",
+            experience: "<1 year",
             percent: "70%"
         },
         {
             item: "Bootstrap",
-            experience: "1 year experience",
+            experience: "1 year",
             percent: "80%"
         },
         {
             item: "SASS",
-            experience: "<1 year experience",
+            experience: "<1 year",
             percent: "70%"
         },
         {
             item: "Express",
-            experience: "<1 year experience",
+            experience: "<1 year",
             percent: "60%"
         },
         {
             item: "JQuery",
-            experience: "<1 year experience",
+            experience: "<1 year",
             percent: "75%"
         },
         {
             item: "Axios, Fetch",
-            experience: "<1 year experience",
+            experience: "<1 year",
             percent: "70%"
         },  
         {
-            item: "Retrofit (Android Dev",
-            experience: "<1 year experience",
+            item: "Retrofit (Android Dev)",
+            experience: "<1 year",
             percent: "70%"
         }, 
     ],
     tools: [
         {
             item: "Git",
-            experience: "1 year experience",
+            experience: "1 year",
             percent: "90%"
         },
         {
             item: "GitHub",
-            experience: "1 year experience",
+            experience: "1 year",
             percent: "90%"
         },
         {
             item: "VSCode/Atom",
-            experience: "1 year experience",
+            experience: "1 year",
             percent: "90%"
         },
         {
             item: "Gimp & Photoshop",
-            experience: "1 year experience",
+            experience: "1 year",
             percent: "80%"
         },
         {
             item: "Chrome Developer Tools",
-            experience: "1 year experience",
+            experience: "1 year",
             percent: "80%"
         },
         {
             item: "NPM",
-            experience: "1 year experience",
+            experience: "1 year",
             percent: "85%"
         },
         {
             item: "MongoDB",
-            experience: "<1 year experience",
+            experience: "<1 year",
             percent: "60%"
         },
         {
             item: "Android Studio",
-            experience: "<1 year experience",
+            experience: "<1 year",
             percent: "75%"
         },
     ],
     other: [
         {
-            item: "Firebase Suite (Func, Database, Hosting, Auth)",
-            experience: "<1 year experience",
+            item: "Firebase Suite",
+            experience: "<1 year",
             percent: "80%"
         },
         {
-            item: "AWS (S3, Cloudfront, Route 53)",
-            experience: "<1 year experience",
+            item: "AWS (S3, CF, R53)",
+            experience: "<1 year",
             percent: "80%"
         },
         {
             item: "Source Tree",
-            experience: "<1 year experience",
+            experience: "<1 year",
             percent: "80%"
         },
         {
             item: "JSON",
-            experience: "1 year experience",
+            experience: "1 year",
             percent: "98%"
         },
         {
             item: "Slack",
-            experience: "<1 year experience",
+            experience: "<1 year",
             percent: "80%"
         },
     ]
